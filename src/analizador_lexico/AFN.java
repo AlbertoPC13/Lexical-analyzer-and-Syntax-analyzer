f37a762b74550077520aac40ff5522176fed2449
AlbertoPC13 <BetucaPC@hotmail.com>
2021-10-26 07:37:30 -0500

    AFN updated

--- src/analizador_lexico/AFN.java
+++ src/analizador_lexico/AFN.java
@@ -334,13 +334,18 @@ public class AFN {
     }
 
     public HashSet<Estado> Mover(Estado Edo, char S) {
+        //Se crea un hashset para los estados a los cuales se puede llegar con el simbolo dado
         HashSet<Estado> C = new HashSet<>();
+        //Se limpia el hashset para iniciar analisis
         C.clear();
 
+        //Se recorren todas las trancisiones del estado recibido
         Edo.getTrans().forEach((t)
                 -> {
+            //Se comprueba si la transicion analizada tiene el simbolo recibido
             Estado Aux = t.GetEdoTrans(S);
             if (Aux != null) {
+                //Si la transicion si era con el simbolo, se agrega al hashset de estados alcanzables
                 C.add(Aux);
             }
         });
@@ -349,15 +354,21 @@ public class AFN {
     }
 
     public HashSet<Estado> Mover(HashSet<Estado> Edos, char S) {
+        //Se crea un hashset para los estados a los cuales se puede llegar con el simbolo dado
         HashSet<Estado> C = new HashSet<>();
+        //Se limpia el hashset para iniciar analisis
         C.clear();
 
+        //Se recorren todas los estados del hashset de estados recibido
         Edos.forEach((Edo)
                 -> {
+            //Se recorren todas las trancisiones del estado actual analizado
             Edo.getTrans().forEach((t)
                     -> {
+                //Se comprueba si la transicion analizada tiene el simbolo recibido
                 Estado Aux = t.GetEdoTrans(S);
                 if (Aux != null) {
+                    //Si la transicion si era con el simbolo, se agrega al hashset de estados alcanzables
                     C.add(Aux);
                 }
             });
@@ -367,127 +378,201 @@ public class AFN {
     }
 
     public HashSet<Estado> Ir_A(HashSet<Estado> Edos, char S) {
+        //Se crea un hashset para los estados a los cuales se puede llegar con la operacion Ir A
+        //de los estados dados con el simbolo recibido
         HashSet<Estado> C = new HashSet<>();
+        //Se limpia el hashset para iniciar analisis
         C.clear();
+        //Se realiza la cerradura epsilon del conjunto de estados retornado por la operacion
+        //Mover aplicada a los estados recibidos con el simbolo recibido
         C = CerrEpsilon(Mover(Edos, S));
 
         return C;
     }
 
     public AFD ConvAFNaAFD() {
+        //Se crean variables para la cardinalidad del alfabeto del nuevo AFD y # de estados de AFD
         int CardAlfabeto, NumEdosAFD;
+        //Iteradores para numero de Conjuntos de estado
         int i, j;
+        //Arreglo para almacenar el alfabeto del AFN
         char[] ArrAlfabeto;
+        //Conjuntos de estados para la conversion
         ConjIj Ij, Ik;
+        //Bandera para indicar si los esados ya estan en el conjunto de estados
         boolean existe;
 
+        //Se crea un hashset para asignar los tokens a los estados de aceptacion del AFD
         HashSet<Estado> ConjAux = new HashSet<>();
+        //Se crea un hashset para el conjunto de estados del nuevo AFD
         HashSet<ConjIj> EdosAFD = new HashSet<>();
+        //Se crea una pila para los conjuntos de estados por analizar
         Queue<ConjIj> EdosSinAnalizar = new LinkedList<>();
 
+        //Se limpia el hashset de estados del AFD y la pila
         EdosAFD.clear();
         EdosSinAnalizar.clear();
 
+        //Se asigna la cardinalidad del alfabeto del AFD
         CardAlfabeto = Alfabeto.size();
+        //Se crea el alfabeto del AFD con la cardinalidad del alfabeto del AFN
         ArrAlfabeto = new char[CardAlfabeto];
 
         i = 0;
+        //Se copia el alfabeto del AFN al alfabeto del nuevo AFD
         for (char c : Alfabeto) {
             ArrAlfabeto[i++] = c;
         }
 
+        //Se inicializa j en 0 para el conjunto I0
         j = 0;
+        //Se crea un nuevo conjunto de estados Ij
         Ij = new ConjIj(CardAlfabeto);
+        //Se realiza la cerradura epsilon del estado inicial
         Ij.ConjI = CerrEpsilon(this.EdoIni);
+        //Se asigna el valor de j como identificador del conjunto de estados Ij
         Ij.j = j;
 
+        //Se agrega el conjunto I0 a los estados del nuevo AFD
         EdosAFD.add(Ij);
+        //Se agrega el conjunto de estados I0 a la pila para ser analizados
         EdosSinAnalizar.add(Ij);
+        //Se incrementa j para la creacion del siguiente conjunto Ij
         j++;
 
+        //Se realiza el ciclo while hasta que la pila de estados sin analizar quede vacia
         while (!EdosSinAnalizar.isEmpty()) {
+            //Se saca el conjunto de estados en el tope de la pila
             Ij = EdosSinAnalizar.poll();
 
+            //Se recorren todos los simbolos del alfabeto
             for (char c : ArrAlfabeto) {
+                //Se crea un nuevo estado Ik
                 Ik = new ConjIj(CardAlfabeto);
+                //Se obtienen los estados para el AFD realizando la operacion Ir A con el simbolo actual del alfabeto
                 Ik.ConjI = Ir_A(Ij.ConjI, c);
 
+                //Si el conjunto Ik es es vacio tras la operacion Ir A
+                //Se continua en el ciclo con el siguiente simbolo del alfabeto 
                 if (Ik.ConjI.isEmpty()) {
                     continue;
                 }
+                //Se asigna la bandera en falso suponiendo que el conjunto no existe
                 existe = false;
 
+                //Se recorren todos los conjuntos de estados que pertenecen a los estados del nuevo AFD
                 for (ConjIj I : EdosAFD) {
+                    //Se compara si el conjunto actual del AFD es igual al conjunti Ik
                     if (I.ConjI.equals(Ik.ConjI)) {
+                        //Si son iguales indica que el conjunto ya existe con la bandera
                         existe = true;
+                        //Se agrega al conjunto Ij una transicion con el simbolo actual con el conjunto Ij
                         Ij.TransicionesAFD[c] = I.j;
                         break;
                     }
                 }
+
+                //Si comprueba si la bander aindica que el conjunto de estados Ik no se encuentran en el conjunto de estados del AFD
                 if (!existe) {
+                    //Se asigna el valor de j al conjunto Ik
                     Ik.j = j;
+                    //Se agrega al conjunto Ij una transicion con el simbolo actual con el conjunto Ik
                     Ij.TransicionesAFD[c] = Ik.j;
+                    //Se agrega el conjunto Ik a el conjunto de estados del nuevo AFD
                     EdosAFD.add(Ik);
+                    //Se agrega el conjunto Ik a la pila de estados sin analizar
                     EdosSinAnalizar.add(Ik);
+                    //Se incrementa j para el siguiente conjunto de estados Ij
                     j++;
                 }
             }
         }
+        
+        //Se asigna el valor de j como el numero de estados del AFD
         NumEdosAFD = j;
 
+        //Se recorren los conjuntos de estados Ij que conforman a los estados del AFD
         for (ConjIj I : EdosAFD) {
+            //Se limpia el conjunto de estados auxiliar
             ConjAux.clear();
+            //Se agrega el conjunto actual de estados de Ij al conjunto auxiliar
             ConjAux.addAll(I.ConjI);
+            //Se realiza la operacion interseccion de conjuntos entre:
+            //Auxiliar y los estados de aceptacion del AFN invocante
             ConjAux.retainAll(this.EdosAcept);
+            //Se comprueba si la interseccion no es vacia
             if (!ConjAux.isEmpty()) {
+                //Si la interseccion no es vacia
+                //Se recorren todos los estados que quedaron en el conjunto auxiliar
                 for (Estado EdoAcept : ConjAux) {
+                    //Se asigna en la utlima columna de transiciones el Token del estado de aceptacion
+                    //Indica que el conjunto Ij tiene estado de aceptacion
                     I.TransicionesAFD[256] = EdoAcept.getToken();
                     break;
                 }
             } else {
+                //Si la interseccion es vacia, se pone -1 indicando que no es estado de aceptacion
                 I.TransicionesAFD[256] = -1;
             }
         }
 
+        //Se crea un nuevo AFD
         AFD AutFD = new AFD();
+        //Se asigna la cardinalidad del alfabeto del AFD
         AutFD.CardAlfabeto = CardAlfabeto;
-
+        //Se crea el arreglo para almacenar el alfabeto
+        AutFD.Alfabeto = new char[CardAlfabeto];
+        
+        //Se crea una nueva tabla de transiciones para el AFD con el numero de estados del AFD y el tamano del codigo ASCII
         AutFD.TablaAFD = new int[EdosAFD.size()][257];
 
+        //Se llena la tabla de transiciones con -1 para inicializar sin transiciones
         for (i = 0; i < EdosAFD.size(); i++) {
             for (j = 0; j < 257; j++) {
                 AutFD.TablaAFD[i][j] = -1;
             }
         }
 
+        //Se copian a la tabla las transiciones de cada conjunto de estados Ij que pertenecen a los estados del AFD 
         for (ConjIj I : EdosAFD) {
             for (j = 0; j < 257; j++) {
                 AutFD.TablaAFD[I.j][j] = I.TransicionesAFD[j];
             }
         }
 
-        ArrAlfabeto = AutFD.Alfabeto;
+        //Se agrega el alfabeto del AFD
+        AutFD.Alfabeto = ArrAlfabeto;
+        //Se agrega el numero de estados del AFD
         AutFD.NumEstados = NumEdosAFD;
 
         return AutFD;
     }
 
     public AFN UnionLexico() {
+        //Se crea un nuevo estado inicial
         Estado ini = new Estado();
         HashSet<AFN> a = new HashSet();
 
+        //Se recorren todos los AFN almacenados
         for (AFN A : ConjDeAFNs) {
+            //Se comprueba si el AFN fue seleccionado para ser unido
             if (A.SeAgregoAFNUnionLexico) {
+                //Se agrega una transicion epsilon del nuevo estado inicial al estado inicial del AFN actual
                 ini.getTrans().add(new Transicion(SimbolosEspeciales.EPSILON, A.EdoIni));
+                //Se agregan los estados del AFN, los estados de aceptacion y el alfabeto a el AFN invocante
                 this.EdosAFN.addAll(A.EdosAFN);
                 this.EdosAcept.addAll(A.EdosAcept);
                 this.Alfabeto.addAll(A.Alfabeto);
+                //Se agrega el estado actual al hashset de los estados que se eliminaran
                 a.add(A);
             }
         }
 
+        //Se elimina del conjunto de AFNs los AFN que entran al AFN del analizador lexico
         ConjDeAFNs.removeAll(a);
+        //Se agrega el nuevo estado inicial al conjunto de estados del AFN invocante
         this.EdosAFN.add(ini);
+        //Se asigna el nuevo estado inicial al AFN invocante
         this.EdoIni = ini;
         return this;
     }
