c59e8544fdae51489f985af3c0084a79ba715b69
AlbertoPC13 <BetucaPC@hotmail.com>
2021-10-27 07:09:22 -0500

    EstadoAnalizLexico class added to project

---
+++ src/analizador_lexico/EstadoAnalizLexico.java
@@ -0,0 +1,27 @@
+package analizador_lexico;
+
+import java.util.Stack;
+
+class EstadoAnalizLexico {
+    public int token, EdoActual, EdoTransicion;
+    
+    public String Lexema;
+    public boolean PasoPorEdoAcept;
+    public int IniLexema, FinLexema, IndiceCaracterActual;
+    public char CaracterActual;
+    public Stack<Integer> Pila = new Stack<>();
+    
+    public EstadoAnalizLexico()
+    {
+        token = 0;
+        EdoActual = 0;
+        EdoTransicion = -1;
+        Lexema = "";
+        PasoPorEdoAcept = false;
+        IniLexema = -1;
+        FinLexema = -1;
+        IndiceCaracterActual = -1;
+        CaracterActual = ' ';
+        Pila.clear();
+    }
+}
