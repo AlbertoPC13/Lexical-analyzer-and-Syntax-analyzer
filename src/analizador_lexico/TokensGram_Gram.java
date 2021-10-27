0fb12c6adeb9d2de8392dd28e9e6474c45db5889
AlbertoPC13 <BetucaPC@hotmail.com>
2021-10-27 04:37:35 -0500

    TokensGram_Gram class created

---
+++ src/analizador_lexico/TokensGram_Gram.java
@@ -0,0 +1,9 @@
+package analizador_lexico;
+
+public class TokensGram_Gram {
+    public static int PC = 10;
+    public static int FLECHA = 20;
+    public static int OR = 30;
+    public static int SIMBOLO = 40;
+    public static int OMITIR = 404;
+}
