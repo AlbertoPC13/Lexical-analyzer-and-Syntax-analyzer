87e7dc87f2022e5c38a5707a12aa9f60f1c8a693
AlbertoPC13 <BetucaPC@hotmail.com>
2021-11-21 20:09:49 -0600

    DescRecG_G & DescRecGram_Gram classes updated

--- src/analizador_lexico/DescRecG_G.java
+++ src/analizador_lexico/DescRecG_G.java
@@ -5,7 +5,9 @@
  */
 package analizador_lexico;
 
+import java.util.HashSet;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -16,8 +18,12 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
     /**
      * Creates new form DescRecG_G
      */
+    DescRecGram_Gram GG;
+    Lista first;
+    
     public DescRecG_G() {
         initComponents();
+        first = new Lista();
         AFD.ConjAFDs.forEach((a) -> {
             AFD1.addItem(String.valueOf(a.IdAFD));
         });
@@ -38,6 +44,18 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
         jLabel2 = new javax.swing.JLabel();
         Epsilon = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        Tabla = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        TablaFirst = new javax.swing.JTable();
+        ListaFirst = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        First = new javax.swing.JButton();
+        Limpiar = new javax.swing.JButton();
+
+        setClosable(true);
+        setIconifiable(true);
+        setTitle("Descenso Recursivo: Gramática de gramáticas");
 
         jLabel1.setText("Selecciona el AFD");
 
@@ -66,41 +84,141 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
 
         jLabel3.setText("Simbolos especiales");
 
+        Tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Simbolo", "Terminal"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        Tabla.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TablaMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(Tabla);
+        if (Tabla.getColumnModel().getColumnCount() > 0) {
+            Tabla.getColumnModel().getColumn(1).setHeaderValue("Terminal");
+        }
+
+        TablaFirst.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Simbolo"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        TablaFirst.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TablaFirstMouseClicked(evt);
+            }
+        });
+        jScrollPane2.setViewportView(TablaFirst);
+
+        ListaFirst.setEditable(false);
+
+        jLabel4.setText("Lista de simbolos para calculo de FIRST");
+
+        First.setText("FIRST");
+        First.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                FirstActionPerformed(evt);
+            }
+        });
+
+        Limpiar.setText("Limpiar");
+        Limpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LimpiarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(68, 68, 68)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel2)
-                            .addComponent(Cadena, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(92, 92, 92)
-                            .addComponent(Epsilon))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addComponent(jLabel1)
-                                        .addComponent(AFD1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGap(79, 79, 79))
-                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                    .addComponent(jLabel3)
-                                    .addGap(72, 72, 72)))))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(Aceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(53, 53, 53)
-                        .addComponent(Cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(63, Short.MAX_VALUE))
+                        .addGap(252, 252, 252)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(Aceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(53, 53, 53)
+                                .addComponent(Cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(8, 8, 8))
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(Cadena, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(92, 92, 92)
+                                    .addComponent(Epsilon))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                                .addComponent(jLabel1)
+                                                .addComponent(AFD1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                            .addGap(79, 79, 79))
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                            .addComponent(jLabel3)
+                                            .addGap(72, 72, 72)))))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(First, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(Limpiar, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(ListaFirst))))
+                .addContainerGap(38, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel4)
+                .addGap(94, 94, 94))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(25, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(20, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(AFD1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -116,7 +234,20 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(Aceptar)
                     .addComponent(Cancelar))
-                .addGap(19, 19, 19))
+                .addGap(24, 24, 24)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ListaFirst, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(First)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(Limpiar)))
+                .addGap(44, 44, 44))
         );
 
         pack();
@@ -140,10 +271,27 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
             }
         }
 
-        DescRecGram_Gram GG = new DescRecGram_Gram(cadena, aux);
+        GG = new DescRecGram_Gram(cadena, aux);
 
         if (GG.AnalizarGramatica()) {
             JOptionPane.showMessageDialog(null, "La cadena es sintácticamente correcta");
+            
+            GG.IdentificarTerminales();
+            DefaultTableModel model = (DefaultTableModel) Tabla.getModel();
+            Object rows[] = new Object[2];
+            GG.Vn.forEach((String s) -> {
+                rows[0] = s;
+                rows[1] = "No terminal";
+                model.addRow(rows);
+            });
+            
+            GG.Vt.forEach((String s) -> {
+                rows[0] = s;
+                rows[1] = "Terminal";
+                model.addRow(rows);
+            });
+            
+            ListaFirst.setText("");
         } else {
             JOptionPane.showMessageDialog(null, "La cadena es sintácticamente incorrecta");
         }
@@ -151,8 +299,42 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_AceptarActionPerformed
 
     private void EpsilonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EpsilonActionPerformed
-        Cadena.setText(Cadena.getText()+'£');
+        Cadena.setText(Cadena.getText() + '£');
     }//GEN-LAST:event_EpsilonActionPerformed
+
+    private void TablaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TablaMouseClicked
+        if(evt.getClickCount() == 2)
+        {
+            int indice = Tabla.getSelectedRow();
+            DefaultTableModel tm = (DefaultTableModel) Tabla.getModel();
+            String simbolo = String.valueOf(tm.getValueAt(indice, 0));
+            Boolean terminal = simbolo.contentEquals("Terminal");
+            ListaFirst.setText(ListaFirst.getText() + simbolo + " ");
+            Nodo dato = new Nodo(simbolo,terminal);
+            first.lista.add(dato);
+        }
+    }//GEN-LAST:event_TablaMouseClicked
+
+    private void TablaFirstMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TablaFirstMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TablaFirstMouseClicked
+
+    private void FirstActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_FirstActionPerformed
+        HashSet<String> Simbolos = GG.First(first);
+        DefaultTableModel tm = (DefaultTableModel) TablaFirst.getModel();
+        Object rows[] = new Object[1];
+        Simbolos.forEach((String s) ->{
+            rows[0] = s;
+            tm.addRow(rows);
+        });
+    }//GEN-LAST:event_FirstActionPerformed
+
+    private void LimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LimpiarActionPerformed
+        ListaFirst.setText("");
+        first.lista.clear();
+        DefaultTableModel tm = (DefaultTableModel) TablaFirst.getModel();
+        tm.setRowCount(0);
+    }//GEN-LAST:event_LimpiarActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -161,8 +343,16 @@ public class DescRecG_G extends javax.swing.JInternalFrame {
     private javax.swing.JTextField Cadena;
     private javax.swing.JButton Cancelar;
     private javax.swing.JButton Epsilon;
+    private javax.swing.JButton First;
+    private javax.swing.JButton Limpiar;
+    private javax.swing.JTextField ListaFirst;
+    private javax.swing.JTable Tabla;
+    private javax.swing.JTable TablaFirst;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
